aee25b0c8442d908306fdf3f2d57f64a82dd4146
Thomas Aven <avensin@hotmail.com>
2019-03-27 18:37:40 +0100

    Initial commit of example mod.

---
+++ SearchChests/ModEntry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace SearchChests
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            this.Monitor.Log("A warning message in the entrypoint.", LogLevel.Warn);
+        }
+
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            this.Monitor.Log("A warning message.", LogLevel.Warn);
+        }
+    }
+}
+
