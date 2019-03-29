9a614277739c04af59381382d758b18e30fbec6c
Thomas Aven <avensin@hotmail.com>
2019-03-29 20:20:12 +0100

    Create own search box.

--- SearchChests/ModEntry.cs
+++ SearchChests/ModEntry.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
@@ -12,11 +13,69 @@ using StardewValley.Menus;
 
 namespace SearchChests
 {
+    internal class SearchTextBox : ChatTextBox
+    {
+        public SearchTextBox(Texture2D textBoxTexture, Texture2D caretTexture,
+                             SpriteFont font, Color textColor)
+            : base(textBoxTexture, caretTexture, font, textColor)
+        {
+        }
+    }
+
+    internal class SearchBox : IClickableMenu
+    {
+        private SearchTextBox searchBox;
+
+        public SearchBox()
+        {
+            Texture2D chatBoxTexture = Game1.content.Load<Texture2D>("LooseSprites\\chatBox");
+            searchBox = new SearchTextBox(chatBoxTexture, null, Game1.smallFont, Color.White);
+
+            searchBox.Width = 896;
+            searchBox.Height = 56;
+            base.width = searchBox.Width;
+            base.height = searchBox.Height;
+
+            base.xPositionOnScreen = 0;
+            base.yPositionOnScreen = 0;
+            Utility.makeSafe(ref base.xPositionOnScreen, ref base.yPositionOnScreen,
+                             searchBox.Width, searchBox.Height);
+            searchBox.X = base.xPositionOnScreen;
+            searchBox.Y = base.yPositionOnScreen;
+
+            searchBox.Selected = false;
+        }
+
+        public override void draw(SpriteBatch b)
+        {
+            searchBox.Draw(b, false);
+        }
+
+        public override void clickAway()
+        {
+            base.clickAway();
+            searchBox.Selected = false;
+        }
+
+        public override void receiveKeyPress(Keys key)
+        {
+            if (key == Keys.Enter)
+                exitThisMenu(true);
+        }
+
+        public void Activate()
+        {
+            searchBox.Selected = true;
+            searchBox.setText("");
+        }
+    }
+
     public class ModEntry : Mod
     {
         internal static IModHelper StaticHelper  { get; private set; }
         internal static IMonitor   StaticMonitor { get; private set; }
 
+        private SearchBox searchBox;
         private ChestSearcher chestSearcher;
 
         public override void Entry(IModHelper helper)
@@ -24,6 +83,7 @@ namespace SearchChests
             StaticHelper = Helper;
             StaticMonitor = Monitor;
 
+            SetupSearchBox();
             chestSearcher = new ChestSearcher();
 
             helper.Events.GameLoop.Saving += this.OnSaveResetSearch;
@@ -39,6 +99,11 @@ namespace SearchChests
         private void OnSaveResetSearch(object sender, SavingEventArgs e)
         {
             chestSearcher.CleanUp();
+        }
+
+        private void SetupSearchBox()
+        {
+            searchBox = new SearchBox();
         }
 
         private void SearchChests()
@@ -59,10 +124,17 @@ namespace SearchChests
 
         private void OnButtonPressedSearch(object sender, ButtonPressedEventArgs e)
         {
-            if (e.Button != Keys.Enter.ToSButton())
+            if (!Context.IsWorldReady)
                 return;
 
-            SearchChests();
+            if (e.Button == Keys.Enter.ToSButton()) {
+                SearchChests();
+            }
+
+            if (e.Button == Keys.S.ToSButton() && e.IsDown(SButton.LeftControl)) {
+                searchBox.Activate();
+                Game1.activeClickableMenu = searchBox;
+            }
         }
 
         private void OnMenuChangedSearch(object sender, MenuChangedEventArgs e)
